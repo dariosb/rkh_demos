e6610d21ec7bd9c8f13bc4885992958e1aa8b021
dariosb <dariosb@users.noreply.github.com>
2016-07-07 13:08:16 +0000

    blinky educiaa demos uncrustified

--- demos/blinky_educiaa/src/bsp.c
+++ demos/blinky_educiaa/src/bsp.c
@@ -1,46 +1,54 @@
-/**
- * \cond
+/*
  *  --------------------------------------------------------------------------
  *
  *                                Framework RKH
  *                                -------------
  *
- * 	          State-machine framework for reactive embedded systems            
- * 	        
- * 	                    Copyright (C) 2010 Leandro Francucci.
- * 	        All rights reserved. Protected by international copyright laws.
+ *            State-machine framework for reactive embedded systems
+ *
+ *                      Copyright (C) 2010 Leandro Francucci.
+ *          All rights reserved. Protected by international copyright laws.
  *
  *
- * 	RKH is free software: you can redistribute it and/or modify it under the 
- * 	terms of the GNU General Public License as published by the Free Software 
- * 	Foundation, either version 3 of the License, or (at your option) any 
- * 	later version.
+ *  RKH is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any
+ *  later version.
  *
- *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY 
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
+ *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  *  more details.
  *
- *  You should have received a copy of the GNU General Public License along 
+ *  You should have received a copy of the GNU General Public License along
  *  with RKH, see copying.txt file.
  *
- * 	Contact information:
- * 	RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
- * 	e-mail:			francuccilea@gmail.com
- *
- *  --------------------------------------------------------------------------
- *  File                     : bsp.c
- *	Last updated for version : v2.4.04
- *	By                       : LF
- *  --------------------------------------------------------------------------
- *  \endcond
- *
- * 	\file
- * 	\ingroup 	prt
- *
- * 	\brief 		BSP for 80x86 OS win32
+ *  Contact information:
+ *  RKH web site:   http://sourceforge.net/projects/rkh-reactivesys/
+ *  e-mail:         francuccilea@gmail.com
+ *  ---------------------------------------------------------------------------
  */
 
+/**
+ *  \file       bsp.c
+ *  \ingroup    bsp
+ *
+ *  \brief      Blinky BSP for EduCIAA.
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2015.10.24  DaBa  v1.0.00  Initial version
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ *  DaBa  Darío Baliña  dariosb@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
 #include "bsp.h"
 #include "blinky.h"
 #include "rkh.h"
@@ -49,212 +57,213 @@
 #include "ciaaPOSIX_string.h" /* <= string header */
 #include "ciaak.h"            /* <= ciaa kernel header */
 
-#define SERIAL_TRACE				1
-
-
 RKH_THIS_MODULE
 
-#if defined( RKH_USE_TRC_SENDER )
+/* ----------------------------- Local macros ------------------------------ */
+#if SERIAL_TRACE == 1
+
+static int32_t utrc;
+    #define TRACE_UART_FD   "/dev/serial/uart/1"
+    #define TRACE_UART_BAUD (void *)ciaaBAUDRATE_115200
+
+/* Trazer Tool COM Port */
+    #define SERIAL_TRACE_OPEN()                                              \
+    {                                                                        \
+        utrc = ciaaPOSIX_open(TRACE_UART_FD, ciaaPOSIX_O_WRONLY);            \
+        ciaaPOSIX_ioctl(utrc,                                                \
+                        ciaaPOSIX_IOCTL_SET_BAUDRATE, TRACE_UART_BAUD);      \
+        ActivateTask(RkhTraceTask);                                          \
+    }
+
+    #define SERIAL_TRACE_CLOSE()  \
+    {                             \
+        ciaaPOSIX_close(utrc);    \
+    }
+
+    #define SERIAL_TRACE_SEND_BLOCK(buf_, len_)     \
+    {                                               \
+        ciaaPOSIX_write(utrc, buf_, len_);          \
+    }
+#else
+    #define SERIAL_TRACE_OPEN()                     (void)0
+    #define SERIAL_TRACE_CLOSE()                    (void)0
+    #define SERIAL_TRACE_SEND(d)                    (void)0
+    #define SERIAL_TRACE_SEND_BLOCK(buf_, len_)     (void)0
+#endif
+
+/* ------------------------------- Constants ------------------------------- */
+#define BLINK_LED_MASK  0x20;
+
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+#if RKH_CFG_TRC_EN == 1
+rui8_t trace_open;
+#endif
+
+/* ---------------------------- Local variables ---------------------------- */
+#if defined(RKH_USE_TRC_SENDER)
 static rui8_t l_isr_tick;
 #endif
 
-static RKH_ROM_STATIC_EVENT( ev_term, TERMINATE );
-
-#define BLINK_LED_MASK	0x20;
 static int32_t fd_out;
 
+#if RKH_CFG_TRC_EN == 1
 static RKH_TS_T bsp_tstamp;
-
-/* 
- * 	For serial trace feature.
- */
-
-#if SERIAL_TRACE == 1
-
-	static int32_t utrc;
-	#define TRACE_UART_FD	"/dev/serial/uart/1"
-	#define TRACE_UART_BAUD	(void *)ciaaBAUDRATE_115200
-
-	/* Trazer Tool COM Port */
-	#define SERIAL_TRACE_OPEN()												 \
-				{														     \
-					utrc = ciaaPOSIX_open(TRACE_UART_FD, ciaaPOSIX_O_WRONLY);\
-					ciaaPOSIX_ioctl(utrc, 									 \
-							ciaaPOSIX_IOCTL_SET_BAUDRATE, TRACE_UART_BAUD);  \
-					ActivateTask( RkhTraceTask );							 \
-				}
-
-	#define SERIAL_TRACE_CLOSE() 						\
-				{										\
-					ciaaPOSIX_close( utrc ); 			\
-				}
-
-	#define SERIAL_TRACE_SEND_BLOCK( buf_, len_ ) 		\
-				{										\
-				   	ciaaPOSIX_write(utrc, buf_,	len_ );	\
-				}
-#else
-	#define SERIAL_TRACE_OPEN()						(void)0
-	#define SERIAL_TRACE_CLOSE()					(void)0
-	#define SERIAL_TRACE_SEND( d )					(void)0
-	#define SERIAL_TRACE_SEND_BLOCK( buf_, len_ )	(void)0
 #endif
 
-void
-rkh_hook_timetick( void )
-{
-	++bsp_tstamp;
-}
+static RKH_ROM_STATIC_EVENT(ev_term, TERMINATE);
 
-void 
-rkh_hook_start( void ) 
-{
-}
-
-
-void 
-rkh_hook_exit( void ) 
-{
-	RKH_TRC_FLUSH();
-}
-
-
-void 
-rkh_assert( RKHROM char * const file, int line )
-{
-	ciaaPOSIX_printf( "RKH_ASSERT: [%d] line from %s "
-						"file\n", line, file );
-	RKH_DIS_INTERRUPT();
-	RKH_TR_FWK_ASSERT( (RKHROM char *)file, __LINE__ );
-/*	__debugbreak();*/
-	rkh_fwk_exit();
-}
-
-
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
 static
 void
-print_banner( void )
+print_banner(void)
 {
-	ciaaPOSIX_printf( "Blinky: a very simple state machine example.\n\n" );
-	ciaaPOSIX_printf( "RKH version      = %s\n", RKH_RELEASE );
-	ciaaPOSIX_printf( "Port version     = %s\n", rkh_get_port_version() );
-	ciaaPOSIX_printf( "Port description = %s\n\n", rkh_get_port_desc() );
-	ciaaPOSIX_printf( "Description: \n\n"
-	 	"The goal of this demo application is to explain how to \n"
-		"represent a \"flat\" state machine and how to use the timer \n"
-		"services using the RKH framework. To do that is proposed a \n"
-		"very simple demo that use one state machine and one timer, \n"
-		"which is shown and explained in the reference manual section \n"
-		"\"Examples\". "
-		"This is the 'hello world' of RKH programming!.\n\n\n" );
+    ciaaPOSIX_printf("Blinky: a very simple state machine example.\n\n");
+    ciaaPOSIX_printf("RKH version      = %s\n", RKH_RELEASE);
+    ciaaPOSIX_printf("Port version     = %s\n", rkh_get_port_version());
+    ciaaPOSIX_printf("Port description = %s\n\n", rkh_get_port_desc());
+    ciaaPOSIX_printf("Description: \n\n"
+                     "The goal of this demo application is to explain how to \n"
+                     "represent a \"flat\" state machine and how to use the timer \n"
+                     "services using the RKH framework. To do that is proposed a \n"
+                     "very simple demo that use one state machine and one timer, \n"
+                     "which is shown and explained in the reference manual section \n"
+                     "\"Examples\". "
+                     "This is the 'hello world' of RKH programming!.\n\n\n");
 
-	ciaaPOSIX_printf( "1.- Press ESC to quit \n\n\n" );
+    ciaaPOSIX_printf("1.- Press ESC to quit \n\n\n");
 }
 
+/* ---------------------------- Global functions --------------------------- */
+void
+rkh_hook_timetick(void)
+{
+#if RKH_CFG_TRC_EN == 1
+    ++bsp_tstamp;
+#endif
+}
+
+void
+rkh_hook_start(void)
+{
+}
+
+void
+rkh_hook_exit(void)
+{
+    RKH_TRC_FLUSH();
+}
+
+void
+rkh_assert(RKHROM char * const file, int line)
+{
+    ciaaPOSIX_printf("RKH_ASSERT: [%d] line from %s "
+                     "file\n", line, file);
+    RKH_DIS_INTERRUPT();
+    RKH_TR_FWK_ASSERT((RKHROM char *)file, __LINE__);
+    rkh_fwk_exit();
+}
 
 #if RKH_CFG_TRC_EN == 1
 
-rui8_t trace_open;
-
-void 
-rkh_trc_open( void )
+void
+rkh_trc_open(void)
 {
-	trace_open = (rui8_t)1;
-	rkh_trc_init();
-	SERIAL_TRACE_OPEN()
-	RKH_TRC_SEND_CFG( BSP_TS_RATE_HZ );
-	RKH_TR_FWK_OBJ( &l_isr_tick );
+    trace_open = (rui8_t)1;
+    rkh_trc_init();
+    SERIAL_TRACE_OPEN()
+    RKH_TRC_SEND_CFG(BSP_TS_RATE_HZ);
+    RKH_TR_FWK_OBJ(&l_isr_tick);
 }
 
-
-void 
-rkh_trc_close( void )
+void
+rkh_trc_close(void)
 {
-	trace_open = (rui8_t)0;
-	SERIAL_TRACE_CLOSE();
+    trace_open = (rui8_t)0;
+    SERIAL_TRACE_CLOSE();
 }
 
-
-RKH_TS_T 
-rkh_trc_getts( void )
+RKH_TS_T
+rkh_trc_getts(void)
 {
-	return bsp_tstamp;
+    return bsp_tstamp;
 }
 
-
-void 
-rkh_trc_flush( void )
+void
+rkh_trc_flush(void)
 {
-	rui8_t *blk;
-	TRCQTY_T nbytes;
-	RKH_SR_ALLOC();
+    rui8_t *blk;
+    TRCQTY_T nbytes;
+    RKH_SR_ALLOC();
 
-	FOREVER
-	{
-		nbytes = 128;
+    FOREVER
+    {
+        nbytes = 128;
 
-		RKH_ENTER_CRITICAL_();
-		blk = rkh_trc_get_block( &nbytes );
-		RKH_EXIT_CRITICAL_();
+        RKH_ENTER_CRITICAL_();
+        blk = rkh_trc_get_block(&nbytes);
+        RKH_EXIT_CRITICAL_();
 
-		if((blk != (rui8_t *)0))
-		{
-			SERIAL_TRACE_SEND_BLOCK( blk, nbytes );
-		}
-		else
-			break;
-	}
+        if ((blk != (rui8_t *)0))
+        {
+            SERIAL_TRACE_SEND_BLOCK(blk, nbytes);
+        }
+        else
+        {
+            break;
+        }
+    }
 }
 #endif
 
-
-void 
-bsp_init( int argc, char *argv[] )
+void
+bsp_init(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
+    (void)argc;
+    (void)argv;
 
-	rkh_fwk_init();
+    rkh_fwk_init();
 
-	fd_out = ciaaPOSIX_open("/dev/dio/out/0", ciaaPOSIX_O_RDWR);
-	print_banner();
+    fd_out = ciaaPOSIX_open("/dev/dio/out/0", ciaaPOSIX_O_RDWR);
+    print_banner();
 
-	RKH_FILTER_ON_GROUP( RKH_TRC_ALL_GROUPS );
-	RKH_FILTER_ON_EVENT( RKH_TRC_ALL_EVENTS );
-	RKH_FILTER_OFF_EVENT( RKH_TE_TMR_TOUT );
-	RKH_FILTER_OFF_EVENT( RKH_TE_SM_STATE );
-	RKH_FILTER_OFF_SMA( blinky );
-	RKH_FILTER_OFF_ALL_SIGNALS();
+    RKH_FILTER_ON_GROUP(RKH_TRC_ALL_GROUPS);
+    RKH_FILTER_ON_EVENT(RKH_TRC_ALL_EVENTS);
+    RKH_FILTER_OFF_EVENT(RKH_TE_TMR_TOUT);
+    RKH_FILTER_OFF_EVENT(RKH_TE_SM_STATE);
+    RKH_FILTER_OFF_SMA(blinky);
+    RKH_FILTER_OFF_ALL_SIGNALS();
 
-	RKH_TRC_OPEN();
+    RKH_TRC_OPEN();
 }
 
-
-void 
-bsp_led_on( void )
+void
+bsp_led_on(void)
 {
-   uint8_t outputs;	
+    uint8_t outputs;
 
-   /* write blinking message */
-   ciaaPOSIX_printf("LED ON\n");
-	
-   /* set output on */
-   ciaaPOSIX_read(fd_out, &outputs, 1);
-   outputs |= BLINK_LED_MASK;
-   ciaaPOSIX_write(fd_out, &outputs, 1);
+    /* write blinking message */
+    ciaaPOSIX_printf("LED ON\n");
+
+    /* set output on */
+    ciaaPOSIX_read(fd_out, &outputs, 1);
+    outputs |= BLINK_LED_MASK;
+    ciaaPOSIX_write(fd_out, &outputs, 1);
 }
 
-
-void 
-bsp_led_off( void )
+void
+bsp_led_off(void)
 {
-   uint8_t outputs;	
+    uint8_t outputs;
 
-   /* write blinking message */
-   ciaaPOSIX_printf("LED OFF\n");
-	
-   /* set output off */
-   ciaaPOSIX_read(fd_out, &outputs, 1);
-   outputs &= ~BLINK_LED_MASK;
-   ciaaPOSIX_write(fd_out, &outputs, 1);
+    /* write blinking message */
+    ciaaPOSIX_printf("LED OFF\n");
+
+    /* set output off */
+    ciaaPOSIX_read(fd_out, &outputs, 1);
+    outputs &= ~BLINK_LED_MASK;
+    ciaaPOSIX_write(fd_out, &outputs, 1);
 }
+
+/* ------------------------------ End of file ------------------------------ */
--- demos/blinky_educiaa/src/oskblky.c
+++ demos/blinky_educiaa/src/oskblky.c
@@ -1,37 +1,86 @@
-/**
- * 	\file		oskdep
- * 	\ingroup 	prt
+/*
+ *  --------------------------------------------------------------------------
  *
- * 	\brief 		Osek Task - RKH State Machine Apps vinculation
+ *                                Framework RKH
+ *                                -------------
+ *
+ *            State-machine framework for reactive embedded systems
+ *
+ *                      Copyright (C) 2010 Leandro Francucci.
+ *          All rights reserved. Protected by international copyright laws.
+ *
+ *
+ *  RKH is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any
+ *  later version.
+ *
+ *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with RKH, see copying.txt file.
+ *
+ *  Contact information:
+ *  RKH web site:   http://sourceforge.net/projects/rkh-reactivesys/
+ *  e-mail:         francuccilea@gmail.com
+ *  ---------------------------------------------------------------------------
  */
 
+/**
+ *  \file       oskblky.c
+ *  \ingroup    prt
+ *
+ *  \brief      Osek Task - RKH State Machine Apps vinculation
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2015.10.24  DaBa  v1.0.00  Initial version
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ *  DaBa  Darío Baliña  dariosb@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
 #include "rkh.h"
 #include "blinky.h"
 #include "bsp.h"
 
-
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
 const SMA_TASK_TBL_T rkh_sma_task_tbl[] =
 {
-	{ RKH_SMA(blinky), BlinkyTask },
-	{ NULL }
+    {RKH_SMA(blinky), BlinkyTask},
+    {NULL}
 };
 
-
-TASK( BlinkyTask )
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+/* ---------------------------- Global functions --------------------------- */
+TASK(BlinkyTask)
 {
-	RKH_TASK_FUNCTION( blinky );
+    RKH_TASK_FUNCTION(blinky);
 }
 
-TASK( RkhTraceTask )
+TASK(RkhTraceTask)
 {
-#if RKH_CFG_TRC_EN == 1	
-	while( trace_open )
-		RKH_TRC_FLUSH();
+#if RKH_CFG_TRC_EN == 1
+    while (trace_open)
+        RKH_TRC_FLUSH();
 #endif
 
-	TerminateTask();
+    TerminateTask();
 }
-
 
 /** \brief Error Hook function
  *
@@ -52,11 +101,15 @@ TASK( RkhTraceTask )
  *
  */
 void
-ErrorHook( void )
+ErrorHook(void)
 {
-   ciaaPOSIX_printf("ErrorHook was called\n");
-   ciaaPOSIX_printf("Service: %d, P1: %d, P2: %d, P3: %d, RET: %d\n",
-		   OSErrorGetServiceId(), OSErrorGetParam1(), OSErrorGetParam2(), 
-		   OSErrorGetParam3(), OSErrorGetRet());
-   ShutdownOS(0);
+    ciaaPOSIX_printf("ErrorHook was called\n");
+    ciaaPOSIX_printf("Service: %d, P1: %d, P2: %d, P3: %d, RET: %d\n",
+                     OSErrorGetServiceId(),
+                     OSErrorGetParam1(), OSErrorGetParam2(),
+                     OSErrorGetParam3(), OSErrorGetRet());
+    ShutdownOS(0);
 }
+
+/* ------------------------------ End of file ------------------------------ */
+
