2aaed2fdf4527255d4df1ef237e3b458afe98371
dariosb <dariosb@users.noreply.github.com>
2016-08-31 13:36:54 +0000

    updates to Aircon

--- demos/cross/aircon/aircon.c
+++ demos/cross/aircon/aircon.c
@@ -23,135 +23,89 @@
 /* ----------------------------- Include files ----------------------------- */
 #include "rkh.h"
 #include "aircon.h"
-#include "bsp.h"
 
 /* ----------------------------- Local macros ------------------------------ */
 /* ------------------------------- Constants ------------------------------- */
 #define MAX_FAN_SPEED   5
 
 /* ---------------------------- Local data types --------------------------- */
+typedef enum
+{
+    POWER_OFF,
+    POWER_ON
+} Power_t;
+
+typedef enum
+{
+    COOLER_MODE,
+    HEATER_MODE
+} Mode_t;
+
 typedef struct AirCon AirCon;
 struct AirCon
 {
     RKH_SMA_T base;
     ruint fanSpeed;
+    Power_t power;
+    Mode_t mode;
 };
 
 /* ---------------------------- Global variables --------------------------- */
 /* ---------------------------- Local variables ---------------------------- */
 /* ----------------------- Local function prototypes ----------------------- */
-/* ============================ Initial action ============================= */
-static void init(AirCon *const me);
-/* ============================ Effect actions ============================= */
-static void setPowerOn(AirCon *const me);
-static void setPowerOff(AirCon *const me);
-static void fanSpeed_inc(AirCon *const me);
-static void fanSpeed_dec(AirCon *const me);
-
-/* ============================= Entry actions ============================= */
-static void setCoolerOn(AirCon *const me);
-static void setHeaterOn(AirCon *const me);
-
-/* ============================= Exit actions ============================== */
-static void setCoolerOff(AirCon *const me);
-static void setHeaterOff(AirCon *const me);
-
-/* ================================ Guards ================================= */
-static rbool_t fanSpeed_isInRange(AirCon *const me);
-static rbool_t fanSpeed_isNotZero(AirCon *const me);
-
-/* ------------------------------- Statechart  ----------------------------- */
-/* ======================== States and pseudostates ======================== */
-RKH_DCLR_BASIC_STATE off, cooler, heater;
-RKH_DCLR_COMP_STATE on;
-
-RKH_CREATE_BASIC_STATE(off, NULL, NULL, RKH_ROOT, NULL);
-RKH_CREATE_TRANS_TABLE(off)
-    RKH_TRREG(evPowerButton, NULL, setPowerOn, &on),
-RKH_END_TRANS_TABLE
-
-RKH_CREATE_COMP_STATE(on, NULL, NULL, RKH_ROOT, &cooler, NULL);
-RKH_CREATE_TRANS_TABLE(on)
-    RKH_TRINT(evFanSpeedUp, fanSpeed_isInRange, fanSpeed_inc),
-    RKH_TRINT(evFanSpeedDown, fanSpeed_isNotZero, fanSpeed_dec),
-    RKH_TRREG(evPowerButton, NULL, setPowerOff, &off),
-RKH_END_TRANS_TABLE
-
-RKH_CREATE_BASIC_STATE(cooler, setCoolerOn, setCoolerOff, &on, NULL);
-RKH_CREATE_TRANS_TABLE(cooler)
-    RKH_TRREG(evModeButton, NULL, NULL, &heater),
-RKH_END_TRANS_TABLE
-
-RKH_CREATE_BASIC_STATE(heater, setHeaterOn, setHeaterOff, &on, NULL);
-RKH_CREATE_TRANS_TABLE(heater)
-    RKH_TRREG(evModeButton, NULL, NULL, &cooler),
-RKH_END_TRANS_TABLE
-
-/* ----------------------------- Active object ----------------------------- */
-RKH_SMA_CREATE(AirCon, airCon, 0, HCAL, &off, init, NULL);
-RKH_SMA_DEF_PTR(airCon);
-
 /* ---------------------------- Local functions ---------------------------- */
 /* ============================ Initial action ============================= */
 static void
 init(AirCon *const me)
 {
-    BSP_AirConInit();
     me->fanSpeed = 0;
+    me->power = POWER_OFF;
+    me->mode = COOLER_MODE;
 }
 
 /* ============================ Effect actions ============================= */
 static void
 setPowerOn(AirCon *const me)
 {
-    BSP_AirConPower(SET_ON);
+    me->power = POWER_ON;
 }
 
 static void
 setPowerOff(AirCon *const me)
 {
-    me->fanSpeed=0;
-
-    BSP_AirConPower(SET_OFF);
-    BSP_setFanSpeed(me->fanSpeed);    
+    me->power = POWER_OFF;
 }
 
 static void
 fanSpeed_inc(AirCon *const me)
 {
-    BSP_setFanSpeed(++me->fanSpeed);
+    ++me->fanSpeed;
 }
 
 static void
 fanSpeed_dec(AirCon *const me)
 {
-    BSP_setFanSpeed(--me->fanSpeed);
+   --me->fanSpeed; 
 }
 
 /* ============================= Entry actions ============================= */
 static void
-setCoolerOn(AirCon *const me)
+setCoolerMode(AirCon *const me)
 {
-    BSP_setCooler(SET_ON);
+    me->mode = COOLER_MODE;
 }
 
 static void
-setHeaterOn(AirCon *const me)
+setHeaterMode(AirCon *const me)
 {
-    BSP_setHeater(SET_ON);
+    me->mode = HEATER_MODE;
 }
 
 /* ============================= Exit actions ============================== */
 static void
-setCoolerOff(AirCon *const me)
+fanSpeed_clear(AirCon *const me)
 {
-    BSP_setCooler(SET_OFF);
-}
-
-static void
-setHeaterOff(AirCon *const me)
-{
-    BSP_setHeater(SET_OFF);
+    me->fanSpeed = 0;
 }
 
 /* ================================ Guards ================================= */
@@ -168,4 +122,35 @@ fanSpeed_isNotZero(AirCon *const me)
 }
 
 /* ---------------------------- Global functions --------------------------- */
+/* ------------------------------- Statechart  ----------------------------- */
+/* ======================== States and pseudostates ======================== */
+RKH_DCLR_BASIC_STATE off, cooler, heater;
+RKH_DCLR_COMP_STATE on;
+
+RKH_CREATE_BASIC_STATE(off, NULL, NULL, RKH_ROOT, NULL);
+RKH_CREATE_TRANS_TABLE(off)
+    RKH_TRREG(evPowerButton, NULL, setPowerOn, &on),
+RKH_END_TRANS_TABLE
+
+RKH_CREATE_COMP_STATE(on, NULL, fanSpeed_clear, RKH_ROOT, &cooler, NULL);
+RKH_CREATE_TRANS_TABLE(on)
+    RKH_TRINT(evFanSpeedUp, fanSpeed_isInRange, fanSpeed_inc),
+    RKH_TRINT(evFanSpeedDown, fanSpeed_isNotZero, fanSpeed_dec),
+    RKH_TRREG(evPowerButton, NULL, setPowerOff, &off),
+RKH_END_TRANS_TABLE
+
+RKH_CREATE_BASIC_STATE(cooler, setCoolerMode, NULL, &on, NULL);
+RKH_CREATE_TRANS_TABLE(cooler)
+    RKH_TRREG(evModeButton, NULL, NULL, &heater),
+RKH_END_TRANS_TABLE
+
+RKH_CREATE_BASIC_STATE(heater, setHeaterMode, NULL, &on, NULL);
+RKH_CREATE_TRANS_TABLE(heater)
+    RKH_TRREG(evModeButton, NULL, NULL, &cooler),
+RKH_END_TRANS_TABLE
+
+/* ----------------------------- Active object ----------------------------- */
+RKH_SMA_CREATE(AirCon, airCon, 0, HCAL, &off, init, NULL);
+RKH_SMA_DEF_PTR(airCon);
+
 /* ------------------------------ End of file ------------------------------ */
